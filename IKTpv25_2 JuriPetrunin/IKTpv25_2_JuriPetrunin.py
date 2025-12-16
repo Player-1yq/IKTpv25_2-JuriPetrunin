a4ef04243c9da9162325fa25a35e3523fba650df
opilane <opilane@HP-CZC2349HT3>
2025-12-16 09:48:15 +0200

    Add project files.

---
+++ IKTpv25_2 JuriPetrunin/IKTpv25_2_JuriPetrunin.py
@@ -0,0 +1,13 @@
+
+palgad=[1000,1200,2500,800,750,1200]
+inimesed=["A","B","C","D","E","F"]
+
+while 1:
+    print("Valige menjüst midagi: ")
+    print("1. Lisage andmed: ")
+    print("2. Suurim palk: ")
+    print("3. Väiksem palk: ")
+    print("4. Otsida palga: ")
+    print("5. Piiratud palk: ")
+    print("6. Keskmine palk: ")
+    print("7. Sorteerida nime jargi: ")
