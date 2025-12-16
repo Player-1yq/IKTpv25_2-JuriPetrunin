f9f891749482463e9ef6eead459ab0f76e7887f1
Player-1yq <petrunin.j@gmail.com>
2025-12-16 09:58:34 +0200

    Update module1.py

--- IKTpv25_2 JuriPetrunin/module1.py
+++ IKTpv25_2 JuriPetrunin/module1.py
@@ -1,2 +1,23 @@
 import random
 import string
+
+def lisa_andmeid():
+    pass
+
+def suurim_palk():
+    pass
+
+def vaikseim_palk():
+    pass
+
+def palga_otsing():
+    pass
+
+def piiratud_palk():
+    pass
+
+def keskmine_palk():
+    pass
+
+def sorteeri_nime_jargi():
+    pass
