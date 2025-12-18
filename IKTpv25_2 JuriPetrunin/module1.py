d292ad917ab7025a724580e6fadae8cf3bc0bea5
Player-1yq <petrunin.j@gmail.com>
2025-12-18 15:01:16 +0200

    Add salary functions and data input handling
    Implemented functions for salary calculations and data handling.

--- IKTpv25_2 JuriPetrunin/module1.py
+++ IKTpv25_2 JuriPetrunin/module1.py
@@ -2,22 +2,44 @@ import random
 import string
 
 def lisa_andmeid():
-    pass
+    for i in range(3):
+        täht=input("Sisestage täht inemese")
+        palk=input("Sisestage listid täht", täht ,"ja", palk)
+        palk.append(palk)
 
-def suurim_palk():
-    pass
+def suurim_palk(inimesed, palgad):
+    max_palk = max(palgad)
+    index = palgad.index(max_palk)
+    return inimesed[index], max_palk
 
-def vaikseim_palk():
-    pass
 
-def palga_otsing():
-    pass
+def vaikseim_palk(inimesed, palgad):
+    min_palk = min(palgad)
+    index = palgad.index(min_palk)
+    return inimesed[index], min_palk
 
-def piiratud_palk():
-    pass
+def palga_otsing(inimesed, palgad, otsitav_nimi):
+    tulemused = []
+    for i in range(inimesed):
+        if inimesed[i] == otsitav_nimi:
+            tulemused.append(palgad[i])
+    return tulemused
 
-def keskmine_palk():
-    pass
 
-def sorteeri_nime_jargi():
-    pass
+def piiratud_palk(inimesed, palgad, min_palk, max_palk):
+    tulemused = []
+    for i in range(len(palgad)):
+        if min_palk <= palgad[i] <= max_palk:
+            tulemused.append((inimesed[i], palgad[i]))
+    return tulemused
+
+
+def keskmine_palk(palgad):
+    if len(palgad) == 0:
+        return 0
+    return sum(palgad) / len(palgad)
+
+
+def sorteeri_nime_jargi(inimesed, palgad):
+
+    return inimesed, palgad
