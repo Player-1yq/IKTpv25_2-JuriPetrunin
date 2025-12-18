936bb5176bcd7618e32abe4c0eb1fcc78f021b53
Player-1yq <petrunin.j@gmail.com>
2025-12-18 15:01:04 +0200

    Import lisa_andmeid function from module1

--- IKTpv25_2 JuriPetrunin/IKTpv25_2_JuriPetrunin.py
+++ IKTpv25_2 JuriPetrunin/IKTpv25_2_JuriPetrunin.py
@@ -1,3 +1,5 @@
+from module1 import lisa_andmeid
+
 
 palgad=[1000,1200,2500,800,750,1200]
 inimesed=["A","B","C","D","E","F"]
@@ -11,3 +13,26 @@ while 1:
     print("5. Piiratud palk: ")
     print("6. Keskmine palk: ")
     print("7. Sorteerida nime jargi: ")
+
+    while True:
+        try:
+            vastus=int(input("Sisestage oma valig menjüst: "))
+            if vastus>0 and vastus<10:
+                break
+        except:
+            print("Viga! Palun sisesta tegevuse number!")
+
+    if vastus==1:
+        lisa_andmeid()
+    if vastus==2:
+       suurim_palk()
+    if vastus==3:
+        vaikseim_palk()
+    if vastus==4:
+        palga_otsing()
+    if vastus==5:
+        piiratud_palk()
+    if vastus==6:
+        keskmine_palk()
+    if vastus==7:
+        sorteeri_nime_jargi()
