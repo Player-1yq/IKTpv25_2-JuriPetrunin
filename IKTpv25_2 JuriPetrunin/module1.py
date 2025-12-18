3d7d542e14b3511c077eb6a023801e9282e787e2
Player-1yq <petrunin.j@gmail.com>
2025-12-18 17:03:21 +0200

    Enhance salary functions with print statements
    Added print statements to display salary information for maximum, minimum, and average salaries, as well as search results.

--- IKTpv25_2 JuriPetrunin/module1.py
+++ IKTpv25_2 JuriPetrunin/module1.py
@@ -10,12 +10,16 @@ def lisa_andmeid():
 def suurim_palk(inimesed, palgad):
     max_palk = max(palgad)
     index = palgad.index(max_palk)
+    print("Suurim palk on:", max_palk)
+    print("Inimene kellel on suurim palk:", inimesed[index])
     return inimesed[index], max_palk
 
 
 def vaikseim_palk(inimesed, palgad):
     min_palk = min(palgad)
     index = palgad.index(min_palk)
+    print("Väikseim palk on:", min_palk)
+    print("Inimene kellel on väikseim palk:", inimesed[index])
     return inimesed[index], min_palk
 
 def palga_otsing(inimesed, palgad, otsitav_nimi):
@@ -23,6 +27,8 @@ def palga_otsing(inimesed, palgad, otsitav_nimi):
     for i in range(inimesed):
         if inimesed[i] == otsitav_nimi:
             tulemused.append(palgad[i])
+    print("Leitud palgad:", tulemused)
+    print("Kokku leitud palkade arv:", len(tulemused))
     return tulemused
 
 
@@ -31,15 +37,18 @@ def piiratud_palk(inimesed, palgad, min_palk, max_palk):
     for i in range(len(palgad)):
         if min_palk <= palgad[i] <= max_palk:
             tulemused.append((inimesed[i], palgad[i]))
+    print("Inimesed palkadega vahemikus:", tulemused)
     return tulemused
 
 
 def keskmine_palk(palgad):
     if len(palgad) == 0:
         return 0
+    print("Keskmine palk on:", sum(palgad) / len(palgad))
     return sum(palgad) / len(palgad)
 
 
 def sorteeri_nime_jargi(inimesed, palgad):
 
     return inimesed, palgad
+
